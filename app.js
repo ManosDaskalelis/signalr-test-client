40193135efe2462520097add754191bf9fbe712c
DASKALELIS_Emmanouil <Manolisdaskalelis@gmail.com>
2026-05-13 13:32:03 +0300

    Added some styling and more test cases

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const connection = new signalR.HubConnectionBuilder()
-  .withUrl("")
+  .withUrl("https://localhost:7274/hubs/chat")
   .configureLogging(signalR.LogLevel.Information)
   .build();
 
@@ -13,17 +13,21 @@ async function start() {
   }
 }
 
-
 connection.on("ReceiveRoomMessage", (data) => {
-    const list = document.getElementById("messageResponses");
-    
-    const messagesArray = Array.isArray(data) ? data : [data];
+  const list = document.getElementById("messageResponses");
 
-    messagesArray.forEach(msg => {
-        const li = document.createElement("li");
-        li.textContent = msg;
-        list.appendChild(li);
-    }); 
+  const messagesArray = Array.isArray(data) ? data : [data];
+
+  messagesArray.forEach((msg) => {
+    const { content, id, roomId, senderId, type, createdAtUtc } = msg;
+    const { reactions } = msg;
+    const emojis = reactions.map((emReact) => emReact.emoji).join(" ");
+
+    const li = document.createElement("li");
+
+    li.textContent = `${content} ${emojis}`;
+    list.appendChild(li);
+  });
 });
 
 connection.onclose(async () => {
@@ -37,7 +41,7 @@ function buttonClicked() {
   joinbutton.addEventListener("click", async () => {
     const roomId = document.getElementById("roomIdInput").value;
     try {
-      await connection.invoke("JoinRoom", roomId);
+      await connection.invoke("SubscribeToRoom", roomId);
       console.log("connected to room");
     } catch (error) {
       console.error(`Error ====> ${error}`);
@@ -48,7 +52,7 @@ function buttonClicked() {
   leavebutton.addEventListener("click", async () => {
     const roomId = document.getElementById("roomIdInput").value;
     try {
-      await connection.invoke("LeaveRoom", roomId);
+      await connection.invoke("UnsubscribeFromRoom", roomId);
       console.log("left the room");
     } catch (error) {
       console.error(`Error ====> ${error}`);
@@ -73,6 +77,57 @@ function buttonClicked() {
     try {
       await connection.invoke("GetRoomMessages", roomId);
       console.log("message received successfully");
+    } catch (error) {
+      console.error(`Error ====> ${error}`);
+    }
+  });
+
+  let addReactionButton = document.getElementById("addReaction");
+  addReactionButton.addEventListener("click", async () => {
+    const messageContent = document.getElementById("messageInput").value;
+    const emojiContent = document.getElementById("emojiInput").value;
+
+    try {
+      await connection.invoke("AddReaction", messageContent, emojiContent);
+      console.log("reaction added");
+    } catch (error) {
+      console.error(`Error ====> ${error}`);
+    }
+  });
+
+  let removeReactionButton = document.getElementById("removeReaction");
+  removeReactionButton.addEventListener("click", async () => {
+    const messageContent = document.getElementById("messageInput").value;
+    const emojiContent = document.getElementById("emojiInput").value;
+
+    try {
+      await connection.invoke("RemoveReaction", messageContent, emojiContent);
+      console.log("reaction removed");
+    } catch (error) {
+      console.error(`Error ====> ${error}`);
+    }
+  });
+
+  let editMessageButton = document.getElementById("editMessageButton");
+  editMessageButton.addEventListener("click", async () => {
+    const messageContent = document.getElementById("messageInput").value;
+    const emojiContent = document.getElementById("emojiInput").value;
+
+    try {
+      await connection.invoke("EditMessage", messageContent, emojiContent);
+      console.log("message edited");
+    } catch (error) {
+      console.error(`Error ====> ${error}`);
+    }
+  });
+
+  let deleteMessageButton = document.getElementById("deleteMessageButton");
+  deleteMessageButton.addEventListener("click", async () => {
+    const messageContent = document.getElementById("messageInput").value;
+
+    try {
+      await connection.invoke("DeleteMessage", messageContent);
+      console.log("message deleted");
     } catch (error) {
       console.error(`Error ====> ${error}`);
     }
